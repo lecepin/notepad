c7dae4304cd2f0b689839a9c7b765420374a6477
lecepin <383810086@qq.com>
2022-06-27 18:41:50 +0800

    Updates

---
+++ precache-manifest.2425bb3d38eec22baef4101b363909b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f3e8890130ddd109ee7ee574af34f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d108fcb35960951e92a",
+    "url": "./static/css/2.ab593844.chunk.css"
+  },
+  {
+    "revision": "e99e90efb13977231d67",
+    "url": "./static/css/main.4afaeac4.chunk.css"
+  },
+  {
+    "revision": "7d108fcb35960951e92a",
+    "url": "./static/js/2.fb3a704f.chunk.js"
+  },
+  {
+    "revision": "e99e90efb13977231d67",
+    "url": "./static/js/main.4765f4f6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,51 @@
+const db = new IDB("lp-notepad", 2);
+
+self.addEventListener("fetch", (e) => {
+  const url = e.request.url;
+
+  if (e.request.method == "POST" && url.indexOf("update-img") > -1) {
+    e.respondWith(
+      e.request.formData().then((data) => {
+        const file = data.get("img");
+        const name = Date.now() + Math.random().toString().substr(2, 4);
+
+        db.put("img", { name, file });
+        return new Response(
+          JSON.stringify({
+            name: name + ".dbimg",
+          })
+        );
+      })
+    );
+  }
+
+  if (e.request.method == "GET" && url.substr(url.length - 6) == ".dbimg") {
+    const name = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
+
+    e.respondWith(
+      db
+        .getAllMatching("img", { index: "name", query: IDBKeyRange.only(name) })
+        .then((data) => {
+          if (data.length) {
+            return new Response(data[0].file);
+          }
+
+          return new Response("", { status: 404 });
+        })
+    );
+  }
+
+  if (e.request.method == "GET" && url.indexOf("get-uploads.json") > -1) {
+    e.respondWith(
+      db.getAllMatching("img").then((data) => {
+        return new Response(JSON.stringify(data));
+      })
+    );
+  }
+
+  if (e.request.method == "DELETE" && url.indexOf("remove-uploads-img") > -1) {
+    const id = new URL(e.request.url).searchParams.get("id");
+
+    id && e.respondWith(db.delete("img", +id).then(() => new Response("")));
+  }
+});
